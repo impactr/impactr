c00e6b59c8690dc07ccc3921065d6289ebaf5e08
Galia <galia@terriblelabs.com>
2014-08-09 13:33:08 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Impactr::Application.config.secret_key_base = '94f999e2021a342e2888423c9973c418ae87aec680fe0548c082c8102ff81fec7e3e8c890069d46835914295675e02cdeb367e992835a23a7f1af379f717a726'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Impactr::Application.config.session_store :cookie_store, key: '_impactr_session'
